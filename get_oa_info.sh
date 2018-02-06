4e03f6d47d189e39cfbea65a1a48ae83934e8e6f
Gini <net.gini@gmail.com>
2018-02-06 14:31:46 +0800

    Update get_oa_info.sh

--- get_oa_info.sh
+++ get_oa_info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#20170807: version 1.0.5 - fixed no blade bug
-#Contact : net.gini@gmail.com
+#20180205: version 1.1.0 - Updated details
+#Contact : gineesh.mada-pparambath@t-systems.com
 
 if [ -z "$1" ]; then
   echo "Enclosure Name is required"
@@ -14,7 +14,16 @@ if [ -n "$DNS_NAME" ];then
   echo "Checking details of $BladeCenter"
 else
   echo "DNS is missing or invalid Enclosure name"
-  exit 1
+  echo "Trying to fetch IP from dhcp file . . ."
+  DHCPIP=$(grep -i "$BladeCenter " /etc/dhcpd.conf|awk '{print $5'}|sed "s/;//g")
+  if [ -z $DHCPIP ]
+  then 
+    echo "No record found to access the enclosure; exiting."
+    exit 1
+  else
+    echo "Found IP for Enclosure $BladeCenter : $DHCPIP. Trying to access . . ."
+    export BladeCenter=$DHCPIP
+  fi
 fi
 #Generate random name for cmds and output file
 RANDOM_commands_for_oa=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
@@ -29,18 +38,26 @@ touch $RANDOM_oa_output
 touch $RANDOM_temp1
 touch $RANDOM_temp2
 echo "show enclosure info" >$RANDOM_commands_for_oa
+#echo "show oa network all" >$RANDOM_commands_for_oa
 echo "show server info all" >>$RANDOM_commands_for_oa
 echo "show interconnect info 1" >>$RANDOM_commands_for_oa
 echo "show interconnect info 2" >>$RANDOM_commands_for_oa
+echo "show oa network 1" >>$RANDOM_commands_for_oa
+echo "show oa info 1" >>$RANDOM_commands_for_oa
+echo "show oa status 1" >>$RANDOM_commands_for_oa
+echo "show oa network 2" >>$RANDOM_commands_for_oa
+echo "show oa info 2" >>$RANDOM_commands_for_oa
+echo "show oa status 2" >>$RANDOM_commands_for_oa
 
 ssh -q -l Administrator $BladeCenter < $RANDOM_commands_for_oa > $RANDOM_oa_output
   newcounter=1
   BAYCOUNT=0
   HEADER01="Bay\tBlade Type\tModel\t\t\tBladeName\tSERIALNUM\tBootMode\tILO IP\t\tFirmware :ROM, ILO, PowerMngmt\n"
-  printf "\nBay\tCPU, Memory\n">$RANDOM_temp1
-  printf "\nBay\tNIC\n">$RANDOM_temp2
+  printf "\n\nBay\tCPU, Memory\n---------------------------------------------\n">$RANDOM_temp1
+  printf "\nBay\tNIC (NIC1, NIC2, iLO NIC)\n---------------------------------------------\n">$RANDOM_temp2
   HEADER02="Enclosure (Serial Number) \t: "
-  HEADER03="\nBladecenter Switches\n"
+  HEADER03="\nBladecenter Switches\n---------------------------------------------\n"
+  HEADER04="\nOnboard Administrators (Name, Network, MAC, Serial, Role, FW)\n---------------------------------------------"
   while read line
   do
     BAYNUM=""
@@ -53,8 +70,8 @@ ssh -q -l Administrator $BladeCenter < $RANDOM_commands_for_oa > $RANDOM_oa_outp
     ILOROM=""
     ILOIP=""
     PMROM=""
-
-    BAYNUM=$(echo "$line"|grep "Server Blade #" )
+    
+	BAYNUM=$(echo "$line"|grep "Server Blade #" )
     if [ -n "$BAYNUM" ];
     then
       if [ -n "$HEADER01" ]; then
@@ -68,27 +85,27 @@ ssh -q -l Administrator $BladeCenter < $RANDOM_commands_for_oa > $RANDOM_oa_outp
       if [ -n "$BLADETYPE" ];
       then
         printf "$BAYCOUNT\t$BLADETYPE\n"
-        # detect the last line in the expansion blade section and exit
-        ENDTHIS=""
-        while read line
+	# detect the last line in the expansion blade section and exit
+	ENDTHIS=""
+	while read line
         do
-          ENDTHIS=$(echo "$line" | egrep "ROM Version:")
-          if [ -n "$ENDTHIS" ]
-          then
-            break
-          fi
-        done
+	  ENDTHIS=$(echo "$line" | egrep "ROM Version:")
+	  if [ -n "$ENDTHIS" ]
+	  then 
+	    break
+	  fi
+	done
       elif [ -n "$BLADETYPENO" ];
       then
         printf "$BAYCOUNT\tNo Blade Installed\n"
-      else
+      else     
         NICALL=""
         NICCOUNT=0
         for i in PRODUCTNAME1 PRODUCTNAME SERIALNUM1 SERIALNUM SERVERNAME1 SERVERNAME ROMVERSION1 ROMVERSION BOOTMODE1 BOOTMODE CPU11 CPU1 CPU21 CPU2 MEMORY1 MEMORY NICA1 NICA ILOROM1 ILOROM ILOIP1 ILOIP LASTLINE1 LASTLINE
-        do
+        do 
           export $i=""
         done
-
+        
         #start collecting data from blade
         while read line
         do
@@ -105,11 +122,11 @@ ssh -q -l Administrator $BladeCenter < $RANDOM_commands_for_oa > $RANDOM_oa_outp
           ILOROM1=$(echo $line | egrep "Firmware Version:")
           ILOIP1=$(echo $line | egrep "IP Address:")
           PMROM1=$(echo $line | egrep "Power Management Controller")
-
+          
           # find the last line of blade set and exist loop for next blade
           # here your decide which text need to be considered as a the end of a blade section
           LASTLINE1=$(echo $line | egrep "Power Management Controller")
-
+          
           if [ -n "$PRODUCTNAME1" ]
           then
             PRODUCTNAME=$(echo "$line"|awk '{print$3,$4,$5}')
@@ -122,7 +139,7 @@ ssh -q -l Administrator $BladeCenter < $RANDOM_commands_for_oa > $RANDOM_oa_outp
           then
             SERVERNAME=$(echo "$line"|awk '{print$3}')
             #echo $SERVERNAME
-                        if [ -z "$SERVERNAME" ]
+		        if [ -z "$SERVERNAME" ]
             then
               SERVERNAME="<No Name>"
             fi
@@ -175,15 +192,15 @@ ssh -q -l Administrator $BladeCenter < $RANDOM_commands_for_oa > $RANDOM_oa_outp
             break
           fi
         done  #end collecting data from blade
-
+        
         #check if same CPU
         if [ "$CPU1" = "$CPU2" ]
         then
           CPUALL=$(echo "2 x $CPU1")
         else
           CPUALL=$(echo "$CPU1, $CPU2")
-        fi
-
+        fi 
+        
         #Print 1st set of data
         printf "$BAYCOUNT\tServer Blade\t$PRODUCTNAME\t$SERVERNAME\t$SERIALNUM\t$BOOTMODE\t\t$ILOIP\t[$ROMVERSION] [$ILOROM] [$PMROM]\n"
         #Print 2nd set of data to temp
@@ -200,7 +217,7 @@ ssh -q -l Administrator $BladeCenter < $RANDOM_commands_for_oa > $RANDOM_oa_outp
         read line
       done
       ENCSERIALNUM=$(echo "$line"|awk '{print$3}' )
-
+      
       if [ -n "$HEADER02" ]; then
         printf "\n========================================================================="
         printf "\n$HEADER02\t$BladeCenter ($ENCSERIALNUM)\n=========================================================================\n"
@@ -240,12 +257,81 @@ ssh -q -l Administrator $BladeCenter < $RANDOM_commands_for_oa > $RANDOM_oa_outp
             SWITCHSERIAL=$(echo "$line"|awk '{print$3}' )
             printf "$SWITCHMODEL\t$SWITCHSERIAL\n"
     fi
+    BAYNUM=$(echo "$line"|grep "Onboard Administrator #. Network Information" )
+    if [ -n "$BAYNUM" ];
+    then
+      if [ -n "$HEADER04" ]; then
+        printf "$HEADER04"
+        HEADER04=""
+      fi
+
+      for i in NAME IPADDR IPMASK IPGW OAMAC NAME1 IPADDR1 IPMASK1 IPGW1 OAMAC1 OASER OASER1 OAFW OAFW1 OAROLE OAROLE1;do export $i="";done
+      OA_EOF=""
+      OA_Temp=""
+      OA_C=0
+      read line
+      NAME="$line"
+      while [ $OA_C -lt 100 ]
+      do
+        read line
+        IPADDR1=$(echo $line | egrep "IPv4 Address:")
+        IPMASK1=$(echo $line | egrep "Netmask:")
+        IPGW1=$(echo $line | egrep "Gateway Address:")
+        OAMAC1=$(echo $line | egrep "MAC Address:")
+        OASER1=$(echo $line | egrep "Serial Number :")
+        OAFW1=$(echo $line | egrep "Firmware Ver. :")
+        OAROLE1=$(echo $line | egrep "Role:")
+        # find the last line of OA set and exit loop
+        # here your decide which text need to be considered as a the end of a OA section 
+        OA_EOF=$(echo "$line"|grep "Diagnostic Status:" )
+        if [ -n "$IPADDR1" ]
+        then
+          IPADDR=$(echo "$line"|awk '{print$3}')
+        elif [ -n "$IPMASK1" ]
+        then
+          IPMASK=$(echo "$line"|awk '{print$2}')
+        elif [ -n "$IPGW1" ]
+        then
+          IPGW=$(echo "$line"|awk '{print$3}')
+        elif [ -n "$OAMAC1" ]
+        then
+          OAMAC=$(echo "$line"|awk '{print$3}')
+        elif [ -n "$OASER1" ]
+        then
+          OASER=$(echo "$line"|awk '{print$4}')
+        elif [ -n "$OAFW1" ]
+        then
+          OAFW="$line" #$(echo "$line"|awk '{print$1}')
+        elif [ -n "$OAROLE1" ]
+        then
+          OAROLE=$(echo "$line"|awk '{print$2}')
+        fi
+
+        if [ -n "$OA_EOF" ];
+        then
+          printf "\n$NAME\t$IPADDR\t$IPMASK\t$IPGW\t$OAMAC\t$OASER\t$OAROLE\t$OAFW"
+          break
+        fi
+        #echo $OA_C
+        OA_C=$(( $OA_C + 1 ))
+      done
+
+	
+
+    fi
 
     newcounter=$(( $newcounter + 1 ))
    done < $RANDOM_oa_output
-   cat $RANDOM_temp1
-   cat $RANDOM_temp2
-rm -rf $RANDOM_oa_output
+   #Display details only if OA login success
+   OALOGINSUCCESS=$(egrep "Enclosure Information" $RANDOM_oa_output)
+    if [ -n "$OALOGINSUCCESS" ];
+    then
+     cat $RANDOM_temp1
+     cat $RANDOM_temp2
+    else
+     echo "Enclosure not Accessible or Reachable"
+    fi
+#rm -rf $RANDOM_oa_output
 rm -rf $RANDOM_commands_for_oa
 rm -rf $RANDOM_temp1
 rm -rf $RANDOM_temp2
