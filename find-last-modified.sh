e560b9c3e932d87bc150448603a3c6dc2ffe7453
ginigangadgaran <net.gini@gmail.com>
2022-07-05 09:44:23 +0800

    update

--- find-last-modified.sh
+++ find-last-modified.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find $1 -type f -exec stat --format '%Y :%y %n' "{}" \; | sort -nr | cut -d: -f2- | head
+find $1 -type f -exec stat --format '%Y :%y %n' "{}" \; | sort -nr | cut -d: -f2- | head -$2
