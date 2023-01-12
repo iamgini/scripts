b9c0a4900e2a5a8ca4fdaa98ad421d7f282948c9
ginigangadgaran <net.gini@gmail.com>
2023-01-13 07:36:05 +0800

    update

--- port-checker/port-checker.sh
+++ port-checker/port-checker.sh
@@ -6,16 +6,18 @@ remote_user=devops
 function port_check {
 
   # open the port on target machine as background job
-  COMMAND_STRING="sudo nc -l $3 &>/dev/null &"
+  COMMAND_STRING="sudo nc -l $entry_port &>/dev/null &"
   # COMMAND_STRING="sleep 10 &>/dev/null &"
   # echo "$COMMAND_STRING"
-  # ssh $remote_user@$2 "hostname -f"
-  echo "Enable listening port on $2:$3"
-  ssh $remote_user@$2 $COMMAND_STRING
+  ssh $remote_user@$entry_destination "hostname -f" < /dev/null
+  echo "Listening port >> $entry_destination:$entry_port"
+  ssh $remote_user@$entry_destination $COMMAND_STRING < /dev/null
   sleep 2
 
   # connect from local machine to test port
-  nc -v $2 $3 &  
+  # nc -v -z $entry_destination $entry_port > port-checker.log  2>&1 
+  nc -v -z $entry_destination $entry_port < /dev/null
+  echo "finished" 
   # LAST_PID=$(echo $!)
   # sleep 5
   # kill $LAST_PID
@@ -41,20 +43,20 @@ while IFS=, read -r entry_source entry_destination entry_port; do
   # echo "Target: $entry_destination"
   printf "\n========= $entry_source -> $entry_destination:$entry_port =========\n"
   # call the function to check
-  # port_check $entry_source $entry_destination $entry_port
+  port_check $entry_source $entry_destination $entry_port
 
-  COMMAND_STRING="sudo nc -l $entry_port &>/dev/null &"
-  # COMMAND_STRING="sleep 10 &>/dev/null &"
-  echo "$COMMAND_STRING"
-  ssh $remote_user@$entry_destination "hostname -f" < /dev/null
-  echo "Listening port >> $entry_destination:$entry_port"
-  ssh $remote_user@$entry_destination $COMMAND_STRING < /dev/null
-  sleep 3
+  # COMMAND_STRING="sudo nc -l $entry_port &>/dev/null &"
+  # # COMMAND_STRING="sleep 10 &>/dev/null &"
+  # # echo "$COMMAND_STRING"
+  # ssh $remote_user@$entry_destination "hostname -f" < /dev/null
+  # echo "Listening port >> $entry_destination:$entry_port"
+  # ssh $remote_user@$entry_destination $COMMAND_STRING < /dev/null
+  # sleep 2
 
-  # connect from local machine to test port
-  # nc -v -z $entry_destination $entry_port > port-checker.log  2>&1 
-  nc -v -z $entry_destination $entry_port < /dev/null
-  echo "finished" 
+  # # connect from local machine to test port
+  # # nc -v -z $entry_destination $entry_port > port-checker.log  2>&1 
+  # nc -v -z $entry_destination $entry_port < /dev/null
+  # echo "finished" 
 
   # if [ "$entry_source" == "" ] || [ "$entry_destination" == "" ] || [ "$entry_port" == "" ]
 	# then
